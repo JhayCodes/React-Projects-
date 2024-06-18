108ebf5cf426655b296a188ceb29dfb8d8d55549
JhayCodes <jhayycodes@gmail.com>
2024-06-18 19:58:38 +0000

    initial commit, styled output

---
+++ React-Jobs/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+
+    </>
+  )
+}
+
+export default App
