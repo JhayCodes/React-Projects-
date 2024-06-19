4004dd2c0e21de1a41d9f67263cd1a896dcde425
JhayCodes <jhayycodes@gmail.com>
2024-06-19 15:08:29 +0000

    created reducer function and a variable containing actions

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -3,9 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Calculator from './components/Calculator'
+import DigitButton from './components/DigitButton'
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+
+}
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+      currentOperand: `${currentOperand || ""}${payload.digit}`
+      }
+
+    case ACTIONS.CHOOSE_OPERATION:
+      return state
+
+    case ACTIONS.CLEAR:
+      return state
+
+    case ACTIONS.DELETE_DIGIT:
+      return state
+
+    case ACTIONS.EVALUATE:
+      return state
+  }
+}
 
 function App() {
-
+  const [(currentOperand, previousOperand, operation), dispatch] = userReducer(reducer)
   return (
     <>
       <Calculator />
